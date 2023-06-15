0b57140cc7d02e69e662f0037a3e0bd9ccb2916f
filipesenaifirjan <filipemattossenai@gmail.com>
2023-06-15 13:19:48 -0300

    Initial commit

---
+++ parking-control/src/main/java/com/parking/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,13 @@
+package com.parking.parkingcontrol.repositories;
+
+import com.parking.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    
+}
